4b689cda337d77af3f4a2285e02d8024b779a2f7
posmansitinjak06-creator <posmansitinjak06@gmail.com>
2026-03-26 16:55:34 +0700

    12S25048

--- src/main.c
+++ src/main.c
@@ -1,15 +1,53 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+    char id [50];
+    char name[100];
+    int quantity;
+    double price;
+} Item;
 
 int main() {
+    Item inventory [100];
+    int count = 0;
     char input[256];
 
-    while (fgets(input, sizeof(input), stdin)) {
-        if (strncmp(input, "---", 3) == 0) {
+    while (scanf("%s", input) != EOF) {
+        if (strcmp(input, "---") == 0) {
             break;
         }
 
-        // TODO: implement logic
+        char *command = strtok (input, "#");
+
+        if (strcmp(command, "receive") == 0) {
+            char *id = strtok(NULL, "#");
+            char *name = strtok(NULL, "#");
+            char *qty_str = strtok(NULL, "#");
+            char *price_str = strtok(NULL, "#");
+            
+            if (id && name && qty_str && price_str) {
+                int exists = 0;
+                for (int i = 0; i < count; i++) {
+                    if (strcmp(inventory[i].id, id) == 0 ) {
+                        exists = 1;
+                        break;
+                    }
+                }
+                if (!exists) {
+                    strcpy(inventory[count].id, id);
+                    strcpy(inventory[count].name, name);
+                    inventory[count].quantity = atoi(qty_str);
+                    inventory[count].price = atof(price_str);
+                    count++;
+                }
+            }
+        } else if (strcmp(command, "report") == 0) {
+            for (int i = 0; i < count; i++) {
+                printf("%s|%s|%d|%1.f\n", inventory[i].id, inventory[i].name, inventory[i].quantity, inventory[i].price);
+            }
+        }
     }
 
     return 0;
